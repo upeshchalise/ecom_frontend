5feacc81b9485c5184cdecb8e3b6c656dcfa168e
upeshchalise <upeshchalise@gmail.com>
2025-08-24 22:32:45 +0545

    analytics page --progress

--- app/(admin)/admin/analytics/page.tsx
+++ app/(admin)/admin/analytics/page.tsx
@@ -1,5 +1,21 @@
+import AnalyticsCard from "@/components/common/AnalyticsCard"
+
 const AdminSalesAnalytics = () => {
-    return <section>THis is the analytics paege</section>
+    return <section id="thrift-analytics">
+        {/* analytics content */}
+        <div>
+            <h1 className="text-4xl">📊 Analytics Dashboard</h1>
+            <div className="flex gap-5 mb-30">
+                <AnalyticsCard title="Total Sales" amount={12430} />
+                <AnalyticsCard title="Total Orders" amount={1234} />
+                <AnalyticsCard title="Total Customers" amount={567} />
+                <AnalyticsCard title="Avg Order Value" amount={45.2} />
+            </div>
+        </div>
+        {/* sales overview */}
+
+        {/* top categories */}
+    </section>
 }
 
 export default AdminSalesAnalytics
---
+++ components/common/AnalyticsCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type AnalyticsCardProps = {
+  title: string;
+  amount: number | string;
+};
+
+const AnalyticsCard: React.FC<AnalyticsCardProps> = ({ title, amount }) => {
+  return (
+    <div className="grow bg-white p-5 rounded-xl text-center shadow-[0px_2px_8px_rgba(0,0,0,0.1)]">
+      <h3 className="text-lg text-[#666]">{title}</h3>
+      <p className="text-xl font-bold text-[#333]">${amount}</p>
+    </div>
+  );
+};
+
+export default AnalyticsCard;
